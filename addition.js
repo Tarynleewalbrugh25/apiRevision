da3c80dfcfd29da282b03c8b8a6f83f1c036b2bb
Tarynleewalbrugh25 <walbrught@gmail.com>
2024-02-07 10:05:02 +0200

    Addition

--- addition.js
+++ addition.js
@@ -1,1 +1,12 @@
-console.log('Addition');
+// function addition(a, b) {               //this is if you want to export multiple functions at the same time 
+//     return a + b 
+// }
+
+// module.exports = {
+//     addition
+// }
+
+exports.addition = (a, b)=> {
+    return a+ b
+}
+
